84182d520d7b259399335de6884a83a9f32c2d24
Aditya Donapati <adityamdonapati@gmail.com>
2024-10-23 22:07:57 +0530

    flutter for web initialization

--- remote_sensing/lib/main.dart
+++ remote_sensing/lib/main.dart
@@ -4,10 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:remote_sensing/HomePage.dart';
 import 'package:remote_sensing/Login.dart';
 import 'package:remote_sensing/Signup.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
